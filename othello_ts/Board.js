65aeb57ab626c5ece62d4ace1accc6eff60cd90a
daigan <daigan47@gmail.com>
2020-01-24 00:39:11 +0900

    0で初期化

--- othello_ts/Board.js
+++ othello_ts/Board.js
@@ -2,11 +2,15 @@ var Board = /** @class */ (function () {
     function Board() {
         this.height = 8;
         this.width = 8;
-        this.board = new Array(this.height * this.width);
+        this.board = new Array(this.height);
         document.writeln(String(this.board.length));
+        for (var _i = 0, _a = this.board; _i < _a.length; _i++) {
+            var item = _a[_i];
+            item = new Array(this.width);
+            document.writeln(String(item.length));
+        }
     }
     return Board;
 }());
-document.writeln("ababa");
-var board = new Board();
+var board2 = new Board();
 //# sourceMappingURL=Board.js.map
--- othello_ts/Board.ts
+++ othello_ts/Board.ts
@@ -1,14 +1,27 @@
 ﻿class Board
 {
-    height: number;
+    height:number;
     width: number;
-    board: number[];
+    board: number[][];
     constructor()
     {
         this.height = 8;
         this.width = 8;
-        this.board = new Array(this.height * this.width);
+        this.board = new Array(this.height);
         document.writeln(String(this.board.length));
+        for (let i of this.board)
+        {
+            i = new Array(this.width);
+            for (let j of i)
+            {
+                j = 0;
+            }
+            //document.writeln(String(i.length));
+        }
+    }
+    Draw()
+    {
+
     }
 }
-let board = new Board();
+let board2 = new Board();

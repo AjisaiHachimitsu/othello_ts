78ece6a5afc859f573a1a3b8844ec3371519c454
daigan <daigan47@gmail.com>
2020-01-24 01:14:03 +0900

    画面表示

--- othello_ts/Board.js
+++ othello_ts/Board.js
@@ -3,14 +3,35 @@ var Board = /** @class */ (function () {
         this.height = 8;
         this.width = 8;
         this.board = new Array(this.height);
-        document.writeln(String(this.board.length));
-        for (var _i = 0, _a = this.board; _i < _a.length; _i++) {
-            var item = _a[_i];
-            item = new Array(this.width);
-            document.writeln(String(item.length));
+        for (var i = 0; i < this.board.length; i++) {
+            this.board[i] = new Array(this.width);
+            for (var j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = 0;
+            }
         }
     }
+    Board.prototype.Draw = function (table) {
+        for (var i = 0; i <= this.height; i++) {
+            table.insertRow(-1);
+            for (var j = 0; j <= this.width; j++) {
+                table.rows[i].insertCell(-1);
+                var cell = table.rows[i].cells[j];
+                if (i == 0) {
+                    if (j != 0)
+                        cell.innerHTML = String(j);
+                }
+                else if (j == 0) {
+                    cell.innerHTML = String(i);
+                }
+                else {
+                    cell.innerHTML = String(this.board[i - 1][j - 1]);
+                }
+            }
+        }
+    };
     return Board;
 }());
 var board2 = new Board();
+var Table = document.getElementById("table");
+board2.Draw(Table);
 //# sourceMappingURL=Board.js.map
--- othello_ts/Board.ts
+++ othello_ts/Board.ts
@@ -1,27 +1,48 @@
 ﻿class Board
 {
-    height:number;
-    width: number;
+    readonly height:number;
+    readonly width: number;
     board: number[][];
     constructor()
     {
         this.height = 8;
         this.width = 8;
         this.board = new Array(this.height);
-        document.writeln(String(this.board.length));
-        for (let i of this.board)
+        for (let i = 0; i < this.board.length;i++)
         {
-            i = new Array(this.width);
-            for (let j of i)
+            this.board[i] = new Array(this.width);
+            for (let j = 0; j < this.board[i].length;j++)
             {
-                j = 0;
+                this.board[i][j] = 0;
             }
-            //document.writeln(String(i.length));
         }
     }
-    Draw()
+    Draw(table:HTMLTableElement)
     {
-
+        for (let i = 0; i <= this.height; i++)
+        {
+            table.insertRow(-1);
+            for (let j = 0; j <= this.width; j++)
+            {
+                table.rows[i].insertCell(-1);
+                let cell = table.rows[i].cells[j];
+                if (i == 0)
+                {
+                    if (j != 0)
+                        cell.innerHTML = String(j);
+                }
+                else if (j == 0)
+                {
+                    cell.innerHTML = String(i);
+                }
+                else
+                {
+                    cell.innerHTML = String(this.board[i-1][j-1]);
+                }
+            }
+        }
     }
 }
 let board2 = new Board();
+let Table:HTMLTableElement = document.getElementById("table")as HTMLTableElement ;
+board2.Draw(Table);
